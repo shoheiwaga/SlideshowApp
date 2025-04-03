8301f37a12c174f86b099c53585ffd541f9c5dbe
shohei.waga <shohei_waga@jrass.jp>
2025-04-03 15:47:26 +0900

    とりあえず完成

---
+++ SlideshowApp/ImageZoomViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ImageZoomViewController.swift
+//  SlideshowApp
+//
+//  Created by PC-SYSKAI555 on 2025/04/02.
+//
+
+import UIKit
+
+class ImageZoomViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var image: UIImage?
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        imageView.image = image
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -8,12 +8,86 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nextImage: UIButton!
+    @IBOutlet weak var returnImage: UIButton!
+    @IBOutlet weak var playstopImage: UIButton!
+    
+    var timer: Timer!
+    var timer_sec: Float = 0 //timerの初期化
+    var images: [UIImage] = [UIImage(named: "image1.jpg")!,UIImage(named: "image2.jpg")!,UIImage(named: "image3.jpg")!] //画像の配列
+    var nowImage = 0 // 配列の位置
+    var playStatus = true //再生ストップのフラグ
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //imageView.contentMode = UIView.ContentMode.scaleAspectFit
+        imageView.image = images[nowImage]
     }
+    
+    @objc func updateTimer(_ timer: Timer){
+        self.timer_sec += 2
+    }
+    
+    @IBAction func nextImageOn(_ sender: UIButton){
+        nowImage = (nowImage + 1) % images.count //配列要素が配列数で割って0なら最初に戻る
+        imageView.image = images[nowImage]
+    }
+    
+    @IBAction func returnImageOn(_ sender: UIButton){
+        nowImage = (nowImage - 1 + images.count) % images.count // -1にならないように余を配列要素にする
+        imageView.image = images[nowImage]
+    }
+    
+    @IBAction func playstopImageOn(_ sender: UIButton){
+        if playStatus{
+            startimage()
+        }else{
+            stopimage()
+        }
+    }
+    
+    func startimage(){
+        
+        timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(nextImageOn(_: )), userInfo: nil, repeats: true)
+        playStatus = false
+        nextImage.isEnabled = false //進むボタン無効
+        returnImage.isEnabled = false //戻る無効
+        playstopImage.setTitle("停止", for: .normal) //ボタンテキスト変更
+        
+    }
+    
+    func stopimage(){
+        
+        playStatus = true
+        nextImage.isEnabled = true //進むボタン有効
+        returnImage.isEnabled = true //戻る有効
+        playstopImage.setTitle("再生", for: .normal) //ボタンテキスト変更
+        timer?.invalidate() //timer止める
+        
+  
+}
 
+    /*
+    @IBAction func imageTap(_ sender: UITapGestureRecognizer) {
+        
+        performSegue(withIdentifier: "Zoom", sender: self)
+     
+    }
+     */
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        //if segue.identifier == "Zoom" {
+            let imageZoomViewController = segue.destination as! ImageZoomViewController
+                imageZoomViewController.image = images[nowImage]
+        //}
+        //let imagezoomViewController: ImageZoomViewController = segue.destination　ダメだった
+    }
+    
+    @IBAction func unwind(_ segue: UIStoryboardSegue){
+        
+    }
 
 }
 
